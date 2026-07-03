c1f8d1c7bb1d5a5a931097aed4dc79bbef4572b9
tkjnasel-cloud <tkjnasel@gmail.com>
2026-07-03 16:41:16 +0700

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDThhpEqI9YKqwvmJlwGjrE1AP5Aj3pdHc",
+  authDomain: "data-diri-1dd4a.firebaseapp.com",
+  projectId: "data-diri-1dd4a",
+  storageBucket: "data-diri-1dd4a.firebasestorage.app",
+  messagingSenderId: "1091160983226",
+  appId: "1:1091160983226:web:af90ef028bc6291eb72483"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
