2433b52104aeac9b9377a4886803f50d39ea592b
tkjnasel-cloud <tkjnasel@gmail.com>
2026-07-06 14:04:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ function tambahLike() {
         like: firebase.firestore.FieldValue.increment(1)
     });
 }
+
+// menambah komentar
+function kirimkomentar() {
+    const nama = document.getElementById("nama").value;
+    const komentar = document.getElementById("komentar").value;
+
+    if (nama === "" || komentar === "") {
+        alert("isi nama dan komentar terlebih dahulu!");
+        return;
+    }
+
+    db.collection ("komentar").add ({
+        nama: nama,
+        komentar: komentar,
+        waktu: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+        document.getElementById("nama").value = "";
+        document.getElementById("komentar").value = "";
+        alert("komentar berhasil di kirim!");
+    })
+    .catch((error) => {
+        console.error(error);
+        alert("gagal mengirim komentar!")
+    });
+}
+
+db.collection("komentar")
+.order.By("waktu", "desc")
+.onSnapshot((snapshot) => {
+    let html = "";
+
+    snapshot.forEach((doc) => {
+
+        const data = doc.data();
+
+        html +=
+            <div style="border:1px solid #ddd:padding:10px;margin:10px;border-radius:10px;">
+                <b>${data.nama}</b><br>
+                ${data.komentar}
+            </div>
+        ;
+    });
+
+    document.getElementById("daftarkomentar").innerHTML = html;
+
+});
